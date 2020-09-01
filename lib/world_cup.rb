ebdbac35b360f9538c5a76bacf6009e23b245b12
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-01 08:16:16 -0600

    Update active players by position method

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -5,8 +5,28 @@ attr_reader :year, :teams
     @year = year
     @teams = teams
   end
-  
-  def active_players_by_position(argument)
-    return team.players_by_position if !team.eliminated?
+
+  def active_players_by_position(position)
+    all_players = []
+    @teams.find_all do |team|
+      unless team.eliminated?
+        all_players << team.players_by_position(position)
+      end
+    end
+    all_players.flatten
+  end
+
+  def all_players_by_position
+    all_players = {}
+    @teams.each do |team|
+      team.players.each do |player|
+        if all_players[player.position]
+           all_players[player.position] << [position]
+        else
+          all_players[player.position] = [player]
+        end
+      end
+    end
+    all_players
   end
 end
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -48,7 +48,7 @@ class WorldCupTest < Minitest::Test
     assert_equal [france, croatia], world_cup.teams
   end
 
-  def return_active_players_by_position
+  def test_return_active_players_by_position
     france = Team.new("France")
     mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
     pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
@@ -70,5 +70,26 @@ class WorldCupTest < Minitest::Test
     croatia.eliminated = true
 
     assert_equal [pogba], world_cup.active_players_by_position("midfielder")
+  end
+
+  def test_if_it_can_return_all_players_by_position
+    france = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+    france.add_player(mbappe)
+    france.add_player(pogba)
+
+    croatia = Team.new("Croatia")
+    modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    vida = Player.new({name: "Domagoj Vida", position: "defender"})
+
+    croatia.add_player(modric)
+    croatia.add_player(vida)
+
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    expected = {"forward" => [mbappe], "midfielder" => [pogba, modric], "defender" => [vida]}
+    assert_equal expected, world_cup.all_players_by_position
   end
 end
