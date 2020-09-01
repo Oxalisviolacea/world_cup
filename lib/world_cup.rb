08dfd6bcac85ac7e040a8015c9dac60ab55332e0
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-31 22:24:23 -0600

    WIP active players by position

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -5,4 +5,8 @@ attr_reader :year, :teams
     @year = year
     @teams = teams
   end
+  
+  def active_players_by_position(argument)
+    return team.players_by_position if !team.eliminated?
+  end
 end
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -43,8 +43,32 @@ class WorldCupTest < Minitest::Test
     croatia.add_player(vida)
 
     world_cup = WorldCup.new(2018, [france, croatia])
-    
+
     assert_equal 2018, world_cup.year
     assert_equal [france, croatia], world_cup.teams
+  end
+
+  def return_active_players_by_position
+    france = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+    france.add_player(mbappe)
+    france.add_player(pogba)
+
+    croatia = Team.new("Croatia")
+    modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    vida = Player.new({name: "Domagoj Vida", position: "defender"})
+
+    croatia.add_player(modric)
+    croatia.add_player(vida)
+
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    assert_equal [pogba, modric], world_cup.active_players_by_position("midfielder")
+
+    croatia.eliminated = true
+
+    assert_equal [pogba], world_cup.active_players_by_position("midfielder")
   end
 end
